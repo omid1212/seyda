1d17ac62682152a543e38918ad9cfc29225bffe2
omid1212 <omidwezd@mihanmail.ir>
2015-12-13 01:09:41 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     },
     sudo_users = {70480064,158492446,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
-    realm = {},--Realms Id
+    realm = {49763092},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
 An advance Administration bot based on yagop/telegram-bot 
